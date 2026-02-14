bc9a3f5d6799d2a038278533f2d8a976cdee7087
deepthi25p <deepthipathigulla@gmail.com>
2026-02-14 18:26:38 +0530

    added API

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:5000";
+const API = "https://task-manager-tpnf.onrender.com";
 
 async function login() {
   const email = document.getElementById("email").value;
--- public/tasks.js
+++ public/tasks.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:5000";
+const API = "https://task-manager-tpnf.onrender.com";
 
 if (!localStorage.getItem("token")) {
   window.location.href = "index.html";
